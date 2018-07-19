db3b8c2108f9df5bb5772c19bcf6c7284a9a7854
Janell Williams <jcw_124@sbcglobal.net>
2018-07-18 21:37:29 -0700

    try again

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+	<header className = "header">
+		<h1>Tsum Tsum Clicky Game!</h1>
+		<h2>Click on any Tsum Tsum to earn a point </h2>
+		<h2>Don't click on the same picture more than once.</h2>	 
+	</header>
+);
+export default Jumbotron;
